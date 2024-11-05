476b059e30c0c88711cf9ebf607ca75240e2d0b1
radselenium <ajaik@radiant-solns.com>
2024-11-05 13:38:53 +0530

    Update config.js

--- client/src/components/config/config.js
+++ client/src/components/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
 	PORT: process.env.PORT || 7001,
-	URL: process.env.URL || 'https://brave-grass-08c74060f.5.azurestaticapps.net/'	
+	URL: process.env.URL || 'https://astronics-d8h0hwc3cqcqhwbc.eastus-01.azurewebsites.net/'	
 	
 }
