b97a5794f84f1329d5b8915990f808616aa4b874
radselenium <ajaik@radiant-solns.com>
2024-11-28 15:21:09 +0530

    download chart added the code

--- client/src/components/dashboard/chart-reports/MessagePerDay.js
+++ client/src/components/dashboard/chart-reports/MessagePerDay.js
@@ -1,13 +1,28 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend,ResponsiveContainer } from 'recharts';
 import HttpClient from '../../config/HttpConfig';
 import moment from 'moment';
+import * as htmlToImage from 'html-to-image';
+import { saveAs } from 'file-saver';
 
 const MessagePerDay = (props) => {
   const [messagesPerDay, setmessagesperDay] = useState(props.dataForMessagePerDay);
   console.log(messagesPerDay)
+  const chartRef = useRef();
+
+  const downloadChart = () => {
+    htmlToImage.toPng(chartRef.current)
+      .then((dataUrl) => {
+        saveAs(dataUrl, 'MessagePerDay.png'); // saveAs from 'file-saver' for downloading
+      })
+      .catch((err) => {
+        console.error('Failed to download image:', err);
+      });
+  };
   return (
-    <div className="text-center mx-auto">
+	<div>
+	<span title='Download Chart' className="fas fa-download" onClick={downloadChart} style={{ cursor: 'pointer',position:'absolute',top:'33px',right:'18px',color:'rgb(6, 85, 144)' }}></span>
+    <div ref={chartRef} className="text-center mx-auto">
             <ResponsiveContainer width="100%" height={300}>
 				<BarChart width={300} height={300} data={props.dataForMessagePerDay} >
 					<CartesianGrid strokeDasharray="3 3" />
@@ -21,6 +36,7 @@ const MessagePerDay = (props) => {
 				</BarChart>
 			</ResponsiveContainer>
         </div>
+		</div>
   );
 };
 
--- client/src/components/dashboard/chart-reports/MessagePerType.js
+++ client/src/components/dashboard/chart-reports/MessagePerType.js
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
 import HttpClient from '../../config/HttpConfig';
+import * as htmlToImage from 'html-to-image';
+import { saveAs } from 'file-saver';
 
 const MessagePerType = (props) => {
     const [data, setData] = useState(props.dataForMessagePerType);
     console.log(props)
-  
+	const chartRef = useRef();
+
+	const downloadChart = () => {
+	  htmlToImage.toPng(chartRef.current)
+		.then((dataUrl) => {
+		  saveAs(dataUrl, 'MessagePerType.png'); // saveAs from 'file-saver' for downloading
+		})
+		.catch((err) => {
+		  console.error('Failed to download image:', err);
+		});
+	};
 
 
     return (
-        <div className="text-center mx-auto">
+		<div>
+		<span title='Download Chart' className="fas fa-download" onClick={downloadChart} style={{ cursor: 'pointer',position:'absolute',top:'33px',right:'18px',color:'rgb(6, 85, 144)' }}></span>
+        <div ref={chartRef} className="text-center mx-auto">
              <ResponsiveContainer width="100%" height={300}>
 				<BarChart width={300} height={300} data={props.dataForMessagePerType} barSize={60 / props.dataForMessagePerType.length}>
 					<CartesianGrid strokeDasharray="3 3" />
@@ -23,6 +37,7 @@ const MessagePerType = (props) => {
 				</BarChart>
 			</ResponsiveContainer> 
         </div>
+		</div>
     );
 };
 
